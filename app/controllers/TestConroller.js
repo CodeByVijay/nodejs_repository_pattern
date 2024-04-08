6e7194951f665b776334b8018a972c6691f91398
Vijay Amule <gitbyvijay@gmail.com>
2024-04-08 16:57:46 +0530

    query params

--- app/controllers/TestConroller.js
+++ app/controllers/TestConroller.js
@@ -3,16 +3,16 @@ const { TestRepository } = require("../Repository/TestRepository");
 const testRepository = new TestRepository();
 
 const testAPI = (req, res) => {
-  testRepository.testAPI(req)
-        .then(result => {
-            
-            res.send(result);
-        })
-        .catch(error => {
-
-            console.error(error);
-            res.status(500).send("Internal Server Error");
-        });
+  const { name } = req.query;
+  testRepository
+    .testAPI(name)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
 };
 
 module.exports = {
